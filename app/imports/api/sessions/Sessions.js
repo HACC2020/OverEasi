0ec893a5dda33f3f4ae13b00ba9aa5b11d10cbf7
Eric Lam <53859607+airyclam@users.noreply.github.com>
2020-10-29 20:59:08 -1000

    meteor methods setup

---
+++ app/imports/api/sessions/Sessions.js
@@ -0,0 +1,26 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/** Encapsulates state and variable values for this collection.
+ * This ain't gonna work.
+ */
+class SessionsCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'SessionsCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      sessionClient: String,
+      sessionId: String,
+    }, { tracker: Tracker });
+    // Ensure collection documents obey schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`; // IDK I think this needs to change
+  }
+}
+
+export const Sessions = new SessionsCollection();
--- app/server/main.js
+++ app/server/main.js
@@ -1,3 +1,4 @@
 import '/imports/startup/server/Accounts';
 import '/imports/startup/server/Publications';
 import '/imports/startup/server/Mongo';
+import '/imports/startup/both/Methods';
