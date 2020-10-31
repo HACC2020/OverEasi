a699263654bb7d09a3c000830eb8e7fd1e1219d0
Eric Lam <53859607+airyclam@users.noreply.github.com>
2020-10-31 12:15:34 -1000

    messagesCollection error

--- app/imports/startup/both/Methods.js
+++ app/imports/startup/both/Methods.js
@@ -3,14 +3,28 @@ import { check } from 'meteor/check';
 import { Intents } from '../../api/intents/Intents';
 import { Messages } from '../../api/messages/Messages';
 import { createSession, queryBot } from './DialogFlowFunctions';
+const dialogflow = require('@google-cloud/dialogflow').v2;
 
+/** Bruh moment
+ * Need to figure out how to make sessionClient accessible to all functions
+ *
+ */
+
+const credentials = {
+  project_id: Meteor.settings.project_id,
+  private_key: Meteor.settings.private_key,
+  client_email: Meteor.settings.client_email,
+};
 const createSessionMethod = 'Session.make';
 const queryBotMethod = 'Query.bot';
 
 Meteor.methods({
   'Session.make'(sessionID) {
     check(sessionID, String);
-    createSession(sessionID);
+    const sessionClient = new dialogflow.SessionsClient({ credentials });
+    const sessionPath = sessionClient.projectAgentSessionPath(credentials.project_id, sessionID);
+    console.log('wassup');
+    // createSession(sessionID);
   },
 });
 /*
--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
-
+import { Messages } from '../../api/messages/Messages';
 /* eslint-disable no-console */
 
 /** Initialize the database with a default data document. */
@@ -14,5 +14,18 @@ if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
+  }
+}
+
+/** Initialize the database with a default data document. */
+function addMessages(data) {
+  Messages.collection.insert(data);
+}
+
+/** Initialize the collection if empty. */
+if (Messages.collection.find().count() === 0) {
+  if (Meteor.settings.defaultMessages) {
+    console.log('Creating default data.');
+    addMessages(Meteor.settings.defaultMessages);
   }
 }
