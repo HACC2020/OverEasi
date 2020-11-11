d4d798140f63e65f6869ca71b199f03a6b5ccb92
Eric Lam <53859607+airyclam@users.noreply.github.com>
2020-11-10 22:08:51 -1000

    Update Methods.js

--- app/imports/startup/both/Methods.js
+++ app/imports/startup/both/Methods.js
@@ -12,26 +12,46 @@ const credentials = {
   project_id: Meteor.settings.project_id,
   private_key: Meteor.settings.private_key,
   client_email: Meteor.settings.client_email,
+  api_key: Meteor.settings.private_key_id,
 };
 const listIntents = 'Intents.list';
-// const queryBotMethod = 'Query.bot';
 
 Meteor.methods({
   'Intents.list'() {
+    const intentsClient = new dialogflow.IntentsClient({ credentials });
+    const projectAgentPath = intentsClient.agentPath(credentials.project_id);
+    const request = {
+      parent: projectAgentPath,
+    };
+    const [response] = intentsClient.listIntents(request);
+    response.forEach(intent => {
+      console.log('====================');
+      console.log(`Intent name: ${intent.name}`);
+      console.log(`Intent display name: ${intent.displayName}`);
+      console.log(`Action: ${intent.action}`);
+      console.log(`Root folowup intent: ${intent.rootFollowupIntentName}`);
+      console.log(`Parent followup intent: ${intent.parentFollowupIntentName}`);
+
+      console.log('Input contexts:');
+      intent.inputContextNames.forEach(inputContextName => {
+        console.log(`\tName: ${inputContextName}`);
+      });
+
+      console.log('Output contexts:');
+      intent.outputContexts.forEach(outputContext => {
+        console.log(`\tName: ${outputContext.name}`);
+      });
+    });
+
+    /*
     try {
-      const result = HTTP.call('GET', 'https://dialogflow.googleapis.com/v2/projects/eric-s-agjx/agent/intents');
+      const result = HTTP.call('GET', `https://dialogflow.googleapis.com/v2/projects/eric-s-agjx/agent/intents?intentView=INTENT_VIEW_UNSPECIFIED&key=[${credentials.api_key}] HTTP/1.1`);
       return console.log(result);
     } catch (e) {
       return console.log('error');
     }
+     */
   },
 });
-/*
-Meteor.methods({
-  'Query.bot'(request, sessionClient) {
-    return sessionClient.detectIntent(request);
-  },
-});
- */
 
 export { listIntents };
