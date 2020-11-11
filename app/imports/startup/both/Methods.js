9e4b3f7ed42c598fcd5a453ef492274d38599119
Eric Lam <53859607+airyclam@users.noreply.github.com>
2020-11-10 21:32:26 -1000

    HTTP request attempt

--- app/imports/startup/both/Methods.js
+++ app/imports/startup/both/Methods.js
@@ -1,8 +1,6 @@
 import { Meteor } from 'meteor/meteor';
-import { check } from 'meteor/check';
 import { _ } from 'meteor/underscore';
-import { Promise } from 'meteor/promise';
-import { Messages } from '../../api/messages/Messages';
+import { HTTP } from 'meteor/http';
 
 const dialogflow = require('@google-cloud/dialogflow').v2;
 
@@ -15,34 +13,16 @@ const credentials = {
   private_key: Meteor.settings.private_key,
   client_email: Meteor.settings.client_email,
 };
-const createSessionMethod = 'Session.make';
+const listIntents = 'Intents.list';
 // const queryBotMethod = 'Query.bot';
 
 Meteor.methods({
-  'Session.make'(sessionID) {
-    check(sessionID, String);
-    const sessionClient = new dialogflow.SessionsClient({ credentials });
-    const sessionPath = sessionClient.projectAgentSessionPath(credentials.project_id, sessionID);
-    const query = _.pluck(Messages.collection.find({ sessionId: sessionID }).fetch(), 'message');
-    const request = {
-      session: sessionPath,
-      queryInput: {
-        text: {
-          text: query,
-          languageCode: 'en-US',
-        },
-      },
-    };
-    const responses = Promise.await(sessionClient.detectIntent(request));
-    console.log('Detected intent');
-    console.log(responses);
-    const result = responses[0].queryResult;
-    console.log(`  Query: ${result.queryText}`);
-    console.log(`  Response: ${result.fulfillmentText}`);
-    if (result.intent) {
-      console.log(`  Intent: ${result.intent.displayName}`);
-    } else {
-      console.log(' No intent matched. ');
+  'Intents.list'() {
+    try {
+      const result = HTTP.call('GET', 'https://dialogflow.googleapis.com/v2/projects/eric-s-agjx/agent/intents');
+      return console.log(result);
+    } catch (e) {
+      return console.log('error');
     }
   },
 });
@@ -54,4 +34,4 @@ Meteor.methods({
 });
  */
 
-export { createSessionMethod };
+export { listIntents };
--- app/imports/startup/client/Startup.jsx
+++ app/imports/startup/client/Startup.jsx
@@ -2,28 +2,17 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import App from '../../ui/layouts/App.jsx';
-import { createSessionMethod } from '../both/Methods';
-
+import listIntents from '../both/Methods';
 // const uuid = require('uuid');
 /** Startup the application by rendering the App layout component. */
 Meteor.startup(() => {
     // this.userId = uuid.v4();
-    // Move this meteor call later. Hopefully moving it fixes everything :)
-    Meteor.call(createSessionMethod, '1234', (error) => {
-      if (error) {
-        console.log('error');
-      } else {
-        console.log('success');
-      }
-    });
-    /*
-    Meteor.call(queryBotMethod, 'hello', (error) => {
-      if (error) {
-        console.log('error');
-      } else {
-        console.log('success');
-      }
-    });
-    */
+  Meteor.call(listIntents, (error) => {
+    if (error) {
+      console.log('error');
+    } else {
+      console.log('success');
+    }
+  });
   render(<App />, document.getElementById('root'));  // eslint-disable-line
 });
